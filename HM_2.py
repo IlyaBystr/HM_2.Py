dd0d81a1b4d5d2e1a2178d1403d0a02333f9f6cb
Ilya <fox367213@mail.com>
2022-12-20 10:28:02 +0300

    end 3 task

--- HM_2.py
+++ HM_2.py
@@ -15,11 +15,23 @@
 
 # 2.Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
 
-x = int(input('Введите число '))
+# x = int(input('Введите число '))
 
-f = 1
-for i in range(x):
-    i = i + 1
-    f = i * f
+# f = 1
+# for i in range(x):
+#     i = i + 1
+#     f = i * f
     
-    print(f)
+#     print(f)
+
+# 3.Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
+
+n = int(input('Введите число ')) 
+
+def sequence(n):
+
+    return[round((1 + 1 / x)**x, 2) 
+    for x in range (1, n + 1)]   
+        
+print(sequence(n))
+print("Сумма равна =", round(sum(sequence(n))))    
