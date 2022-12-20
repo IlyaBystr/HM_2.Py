839f664b12268b403da4ffb721c6b95aa94ed4a8
Ilya <fox367213@mail.com>
2022-12-20 10:22:36 +0300

    end 2 task

--- HM_2.py
+++ HM_2.py
@@ -1,14 +1,25 @@
 # 1.Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
-x = input('Введите число ')
+# x = input('Введите число ')
 
-def summa(x):                           
-    if float(x) < 0:                            
-        x = float(x) * (-1)
-    number = 0
+# def summa(x):                           
+#     if float(x) < 0:                            
+#         x = float(x) * (-1)
+#     number = 0
 
-    for i in str(x):
-        if i != '.':
-            number += int(i)
-    return number
+#     for i in str(x):
+#         if i != '.':
+#             number += int(i)
+#     return number
 
-print(f'Сумма чисел равна {summa(x)}')
+# print(f'Сумма чисел равна {summa(x)}')
+
+# 2.Напишите программу, которая принимает на вход число N и выдает набор произведений чисел от 1 до N.
+
+x = int(input('Введите число '))
+
+f = 1
+for i in range(x):
+    i = i + 1
+    f = i * f
+    
+    print(f)
