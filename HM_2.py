7cc038ca35b04282e717d8d24a2adbfc3c4c75e8
Ilya <fox367213@mail.com>
2022-12-20 10:48:33 +0300

    end 4 task

--- HM_2.py
+++ HM_2.py
@@ -26,12 +26,41 @@
 
 # 3.Задайте список из n чисел последовательности $(1+\frac 1 n)^n$ и выведите на экран их сумму.
 
-n = int(input('Введите число ')) 
+# n = int(input('Введите число ')) 
 
-def sequence(n):
+# def sequence(n):
 
-    return[round((1 + 1 / x)**x, 2) 
-    for x in range (1, n + 1)]   
+#     return[round((1 + 1 / x)**x, 2) 
+#     for x in range (1, n + 1)]   
         
-print(sequence(n))
-print("Сумма равна =", round(sum(sequence(n))))    
+# print(sequence(n))
+# print("Сумма равна =", round(sum(sequence(n))))    
+
+# 4.Задайте список из N элементов, заполненных числами из промежутка [-N, N]. Найдите произведение элементов на указанных позициях. Позиции хранятся в файле file.txt в одной строке одно число.
+
+from random import Random, randint
+
+
+N = int(input('Введите количество элементов '))
+numbers = []
+for i in range(N):
+    numbers.append(randint(-N,N+1))
+    
+print(numbers)
+
+f = open('file.txt', 'w')
+while True:
+    s = input('Укажите позицию для вычисления - ')    #Для выхода из файла file.txt просто нажать Enter
+    if s == "":
+        break
+    f.write(s+"\n")
+f.close()
+
+result = 1
+f = open('file.txt', 'r')
+for line in f:
+    if line == "":
+        break
+    result *= numbers[int(line)]
+f.close()
+print(result)
